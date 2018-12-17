2f92f4de0712d0ba388f9bf8ff517215f1cb7d47
Frank Wang <wonderfulsuccess@163.com>
2018-12-17 16:48:13 +0800

    modifr restart func to support mac

--- project/tools/__init__.py
+++ project/tools/__init__.py
@@ -2,5 +2,6 @@ def restart():
     import os,sys
     print(sys.executable)
     print(*([sys.executable]+sys.argv))
-    os.execl(sys.executable, "python.exe", *sys.argv)
+    # 如果环境中同时安装python2和python3 请修改python为python3
+    os.execl(sys.executable, "python", *sys.argv)
 
